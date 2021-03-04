2aea922a39d69fe8fac09a5a0adaade9f5b2ae47
Trenton Cornwall <trenton434@gmail.com>
2021-03-04 17:34:56 -0600

    clean up comments

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,15 +4,6 @@ import SignIn from "../components/SignIn/";
 export default function Home() {
   //TODO add global state
   //TODO add API to check user
-  // const submit = (e) => {
-  //   e.preventDefault();
-  //   console.log(`username: ${username}, password: ${password}`);
-
-  //   api
-  //     .userLogin({ username: username, password: password })
-  //     .then((res) => console.log(res))
-  //     .catch((err) => console.log(err));
-  // };
 
   return (
     <>
